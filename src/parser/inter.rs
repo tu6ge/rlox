ad7b414d8db8346e26eb2ed97d8e29953f5a2d28
tu6ge <tu6ge@qq.com>
2024-12-18 09:56:52 +0800

    feat: add runtime error

--- src/parser/inter.rs
+++ src/parser/inter.rs
@@ -1,78 +1,99 @@
-use crate::lexer::TokenType::*;
+use crate::lexer::{Token, TokenType::*};
 
 use super::{ast::Expr, LiteralTypes, Visitor};
 
 pub struct Interpreter {}
 
+#[derive(Debug, Clone)]
+enum RuntimeError {
+    String(std::string::String),
+}
+
 impl Interpreter {
     pub fn new() -> Self {
         Self {}
     }
-    fn evaluate(&mut self, ast: &Expr) -> LiteralTypes {
+    fn evaluate(&mut self, ast: &Expr) -> Result<LiteralTypes, RuntimeError> {
         ast.accept(self)
+    }
+    fn error(&self, token: &Token, message: &str) -> RuntimeError {
+        let line = token.line;
+
+        let msg = format!("line:{line} {message}");
+
+        RuntimeError::String(msg)
     }
 }
 
-impl Visitor<LiteralTypes> for Interpreter {
-    fn visit_binary(&mut self, expr: &super::ast::Binary) -> LiteralTypes {
-        let left = self.evaluate(&expr.left);
+impl Visitor<Result<LiteralTypes, RuntimeError>> for Interpreter {
+    fn visit_binary(&mut self, expr: &super::ast::Binary) -> Result<LiteralTypes, RuntimeError> {
+        let left = self.evaluate(&expr.left)?;
 
-        let right = self.evaluate(&expr.right);
+        let right = self.evaluate(&expr.right)?;
 
         match (expr.op.ttype.clone(), left, right) {
             (Plus, LiteralTypes::Number(left), LiteralTypes::Number(right)) => {
-                LiteralTypes::Number(left + right)
+                Ok(LiteralTypes::Number(left + right))
             }
             (Plus, LiteralTypes::String(left), LiteralTypes::String(right)) => {
-                LiteralTypes::String(format!("{}{}", left, right))
+                Ok(LiteralTypes::String(format!("{}{}", left, right)))
             }
+            (Plus, _, _) => Err(self.error(&expr.op, "Operand must be two numbers or two strings")),
             (Minus, LiteralTypes::Number(left), LiteralTypes::Number(right)) => {
-                LiteralTypes::Number(left - right)
+                Ok(LiteralTypes::Number(left - right))
             }
             (Star, LiteralTypes::Number(left), LiteralTypes::Number(right)) => {
-                LiteralTypes::Number(left * right)
+                Ok(LiteralTypes::Number(left * right))
             }
             (Slash, LiteralTypes::Number(left), LiteralTypes::Number(right)) => {
                 if right == 0.0 {
-                    todo!("0 cannot be used as a divisor")
+                    return Err(self.error(&expr.op, "0 cannot be used as a divisor"));
                 }
-                LiteralTypes::Number(left / right)
+                Ok(LiteralTypes::Number(left / right))
             }
-            _ => todo!("complier error"),
+            _ => Err(self.error(&expr.op, "Operand must be two number")),
         }
     }
 
-    fn visit_grouping(&mut self, expr: &super::ast::Grouping) -> LiteralTypes {
+    fn visit_grouping(
+        &mut self,
+        expr: &super::ast::Grouping,
+    ) -> Result<LiteralTypes, RuntimeError> {
         self.evaluate(&expr.expr)
     }
 
-    fn visit_unary(&mut self, expr: &super::ast::Unary) -> LiteralTypes {
-        let value = self.evaluate(&expr.right);
+    fn visit_unary(&mut self, expr: &super::ast::Unary) -> Result<LiteralTypes, RuntimeError> {
+        let value = self.evaluate(&expr.right)?;
 
         if let Minus = expr.op.ttype {
             if let LiteralTypes::Number(f) = value {
-                return LiteralTypes::Number(-f);
+                return Ok(LiteralTypes::Number(-f));
+            } else {
+                return Err(self.error(&expr.op, "Minus must be a number"));
             }
         }
         if let Bang = expr.op.ttype {
-            return match value {
+            return Ok(match value {
                 LiteralTypes::Bool(b) => LiteralTypes::Bool(!b),
                 LiteralTypes::Nil => LiteralTypes::Bool(true),
                 _ => LiteralTypes::Bool(false),
-            };
+            });
         }
 
-        LiteralTypes::Nil
+        Ok(LiteralTypes::Nil)
     }
 
-    fn visit_literal(&self, expr: &LiteralTypes) -> LiteralTypes {
-        expr.clone()
+    fn visit_literal(&self, expr: &LiteralTypes) -> Result<LiteralTypes, RuntimeError> {
+        Ok(expr.clone())
     }
 
-    fn visit_comparison(&mut self, expr: &super::ast::Comparison) -> LiteralTypes {
-        let left = self.evaluate(&expr.left);
+    fn visit_comparison(
+        &mut self,
+        expr: &super::ast::Comparison,
+    ) -> Result<LiteralTypes, RuntimeError> {
+        let left = self.evaluate(&expr.left)?;
 
-        let right = self.evaluate(&expr.right);
+        let right = self.evaluate(&expr.right)?;
 
         let bool = match (expr.op.ttype.clone(), left, right) {
             (Greater, LiteralTypes::Number(left), LiteralTypes::Number(right)) => left > right,
@@ -86,7 +107,7 @@ impl Visitor<LiteralTypes> for Interpreter {
             _ => false,
         };
 
-        LiteralTypes::Bool(bool)
+        Ok(LiteralTypes::Bool(bool))
     }
 }
 
@@ -103,7 +124,7 @@ mod tests {
         let ast = Parser::new(source).equality().unwrap();
 
         let mut inter = Interpreter::new();
-        inter.evaluate(&ast)
+        inter.evaluate(&ast).unwrap()
     }
 
     #[test]
